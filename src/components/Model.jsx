e67d33a8350c76e59a4a5ee5d7b1c11f0e522eb5
unknown <herbertntim15@gmail.com>
2024-03-23 00:39:59 +0000

    starting sentry

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -8,6 +8,7 @@ import { View } from '@react-three/drei'
 
 import ModelView from './ModelView'
 import { models, sizes } from '../constants'
+import { animateWithGsapTimeline } from '../utils/animations'
 
 const Model = () => {
   const [size, setSize] = useState('small')
@@ -29,6 +30,25 @@ const Model = () => {
   // rotation
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+
+  // animation to the model view
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
+
+    if(size ==='small') {
+      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
 
   useGSAP(() => {
     gsap.to('#heading', {opacity: 1, y: 0})
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,47 @@
-const ModelView = () => {
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import { Suspense } from "react"
+import IPhone from './IPhone'
+import * as THREE from 'three'
+import Loader from "./Loader"
+
+const ModelView = ({ index, groupRef, gsapType, item, size, controlRef, setRotationState }) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`absolute h-full w-full ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* setting lights */}
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      {/* Orbit control to move the iPhone model */}
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      {/* Rendering the iPhone model */}
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+        <Suspense fallback={<Loader />}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
   )
 }
 
